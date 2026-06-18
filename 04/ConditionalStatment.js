6e04d0870947624f050266d089c5081a87c8dd4c
SushantYadav18 <shusant.kumar2bca@gmail.com>
2026-06-19 03:12:21 +0545

    Conditional Statement Switch Completed

---
+++ 04/ConditionalStatment.js
@@ -0,0 +1,50 @@
+ const age =50;
+
+if (age>=80) {
+    console.log('You are capable for Vote')
+} else {
+    console.log('Not for Vote')
+    
+}
+
+// Used ternary operators for one condition statement
+const mark=50
+const result = mark>=35 ? 'Congratulation!! You are Pass': 'Sorry You are Fail. '; //ternary operator
+console.log(result)
+
+
+
+//Switch Case
+
+const choice =6;
+
+ switch(choice){
+    case 1: console.log('Add')
+    break;
+    case 2: console.log("sub")
+    break;
+
+    default:console.log('IDK')
+ }
+
+ //Another Example
+
+ const a=30;
+ const opt='+'
+ const b=5
+
+ switch (opt) {
+    case '+':
+        console.log(a+b)
+        break;
+ case '-':
+        console.log(a-b)
+        break;
+ case '*':
+        console.log(a*b)
+        break;
+
+
+    default: console.log("You choice invalid number")
+        break;
+ }
