aa5692c64929a776a529aa181764adfadfeb38bd
SushantYadav18 <shusant.kumar2bca@gmail.com>
2026-06-19 01:41:21 +0545

    Class 2 variable and Class 3 DataTypes Completed

---
+++ 02/Variable.js
@@ -0,0 +1,39 @@
+// var a = 20;
+// var b= 30;
+
+// console.log(a+b);
+
+// let uses
+if (true) {
+    let a =2;
+   
+}
+ console.log(a) // can't access the let variable bcz let variable is local access 
+
+
+//const uses
+
+if (true) {
+    const a =25;
+   
+}
+ console.log(a) // can't access the const variable bcz const a variable is local access 
+
+
+
+// var uses 
+
+ if (true) {
+    var a =25;
+   
+}
+ console.log(a) // can access the var variable bcz const a variable is global access 
+
+
+var a =25;
+   a=30;
+console.log(a)
+
+
+const age = 25;
+console.log(age +10);
---
+++ 03/dataTypes.js
@@ -0,0 +1,42 @@
+console.log('this is Datatypes ')
+
+//1.Numbers
+
+let num = 25;
+let num1= 30;
+ console.log(num + num1);
+
+
+ //Strings
+
+ let firstName=('Shusant');
+  let lastNAme =('Yadav');
+   
+  console.log(firstName + ' ' + lastNAme)
+
+  // booleans
+
+  let lastLogin = true;
+  let previous = false;
+
+  console.log(lastLogin , previous)
+
+  //Null Means any (Like : number, string ,booleans)
+
+  let age=null;
+
+  console.log(age)
+
+  //objects
+
+  const Details= {
+       name:'sushant',
+       age:25 ,
+       address:'kathamandu',
+       student: true ,
+       experience:null
+  }
+
+  console.log(Details) 
+  console.log(Details.address)
+  console.log(Details.experience)
