83c0992df8105b6cddd8787dc526ea87581f667f
SushantYadav18 <shusant.kumar2bca@gmail.com>
2026-06-23 18:44:05 +0545

    new topic logical operator learn

---
+++ 05/Logical.js
@@ -0,0 +1,28 @@
+// console.log("This is logical");
+
+// const mark = 20;
+
+// const result = mark >= 30 ? 'pass' : 'fail';
+
+// console.log(result)
+
+
+// const day = 5;
+
+// switch (day) {
+//     case 1: console.log('Sunday')
+//         break;
+//     case 2: console.log('Monday')
+//         break;
+//     case 3: console.log('Tuesday')
+//         break;
+//     case 4: console.log('Wednesday')
+//         break;
+//     case 5: console.log('Friday')
+//         break;
+
+//     default: console.log('Invalid Number');
+//         break;
+// }
+
+
