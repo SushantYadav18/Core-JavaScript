2ea2d580eb8b49dc2d812c5772d10e1690ddde27
SushantYadav18 <shusant.kumar2bca@gmail.com>
2026-06-25 00:20:27 +0545

    done loop concept

--- 06/Loops.js
+++ 06/Loops.js
@@ -1,7 +1,29 @@
 
 
 
-  
+  //for loop- used in if you know how many times print(show)
 for(let j=0; j<10; j++) {
-  console.log(j);
+  console.log('This will run 10 times' + j);
 }
+
+
+//while loop
+
+let i = 1;
+
+while (i <= 5) {
+    console.log(i);
+    i++;
+}
+
+//do while
+
+ const k=0;
+
+ do {
+    console.log(k);
+    k++;
+} while (k <= 5);
+
+
+
